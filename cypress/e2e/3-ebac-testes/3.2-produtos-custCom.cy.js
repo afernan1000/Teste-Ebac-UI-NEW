15fc5aa589dd775dcdfa5f87b9b515abc0c2f502
André Fernandes <105398833+afernan1000@users.noreply.github.com>
2023-03-06 18:07:26 -0300

    Update tst com variável~

--- cypress/e2e/3-ebac-testes/3.2-produtos-custCom.cy.js
+++ cypress/e2e/3-ebac-testes/3.2-produtos-custCom.cy.js
@@ -38,4 +38,14 @@ describe('Funcionalidade Página de Produto', () => {
         cy.get('.woocommerce-message').should('contain', 3 + ' × “Abominable Hoodie”')
     });
 
+    it('Deve selecionar um produto da lista - Usando comandos customizados + Variável', () => {
+        // CRIANDO VARIÁVEL
+        var quantidade = 3
+        // ESCOLHENDO UM PRODUTO
+        cy.addProdutos('Abominable Hoodie', 'L', 'Green', quantidade)
+        // VALIDAÇÃO COM VALOR ESPECÍFICO DA QUANTIDADE
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Abominable Hoodie”')
+    });
+
 })
