1cbd48c76bc88b547a3acd140a5543b5fd44c96c
André Fernandes <105398833+afernan1000@users.noreply.github.com>
2023-03-06 18:05:03 -0300

    Update tst endereço validação

--- cypress/e2e/3-ebac-testes/4.0-endereco.cy.js
+++ cypress/e2e/3-ebac-testes/4.0-endereco.cy.js
@@ -4,15 +4,20 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 
     beforeEach(() => {
         cy.visit('minha conta')
+        // APONTANDO PARA O PERFIL EM FIXTURE E CRIANDO VARIAVEL DADOS
         cy.fixture('perfil').then(dados => {
-            cy.login(dados.usuario, dados.senha)
+            // FAZENDO LOGIN COM COMANDOS CUSTOMIZADOS E COM A VARIAVEL DADOS
+            cy.login(dados.usuario, dados.senha)            
         })
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        // LOGIN
-
-        //CASDATRO DE ENDEREÇO
-
+        // VALIDAÇÃO PARCIAL
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá,')
+        // OPÇÃO ENDEREÇOS
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
+        // VALIDAÇÃO FINAL
+        cy.get('h2').should('contain', 'My Addresses')
     });
 });
