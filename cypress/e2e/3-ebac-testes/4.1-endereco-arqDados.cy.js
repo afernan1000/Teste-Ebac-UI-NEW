4dfd6b875a2d0c644d5c8b2bd36090156b243947
André Fernandes <105398833+afernan1000@users.noreply.github.com>
2023-03-21 00:14:25 -0300

    Aula Lista de Dados

---
+++ cypress/e2e/3-ebac-testes/4.1-endereco-arqDados.cy.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+
+// FAZENDO A IMPORTAÇÃO DO PAGE OBJECTS DA PASTA SUPPORT
+import enderecoPage from "../../support/page-objects/endereco.page";
+
+// CRIando A MINHA CONSTANTE
+const dadosEnredeco = require('../../fixtures/endereco.json')
+
+describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
+
+    beforeEach(() => {
+        cy.visit('minha conta')
+        // APONTANDO PARA O PERFIL EM FIXTURE E CRIANDO VARIAVEL DADOS
+        cy.fixture('perfil').then(dados => {
+            // FAZENDO LOGIN COM COMANDOS CUSTOMIZADOS E COM A VARIAVEL DADOS
+            cy.login(dados.usuario, dados.senha)
+        })
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
+        // APONTO PARA MEU MÉTODO TODOS OS PARAMETOS EXIGIDOS DO ARQUIVO PAGE-OBJECTS
+        enderecoPage.editarEnderecoFaturamento('André', 'Fernandes', 'EBAC', 'Brasil', 'Av. Rio Branco', '2001', 'São Paulo', 'São Paulo', '01000100', '1199999999', 'andre@ebac.com')
+        // VALIDAÇÃO
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+    })
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - Usando Lista de Dados', () => {
+        // APONTO PARA MEU MÉTODO USAR A LISTA DE DADOS NO ARQUIVO ENDEREÇO EM FIXTURES
+        // ATENÇÃO PARA IMPORTAR A LISTA DE INICINADO EM 0,1,2...N
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEnredeco[1].nome,
+            dadosEnredeco[1].sobrenome,
+            dadosEnredeco[1].empresa,
+            dadosEnredeco[1].pais,
+            dadosEnredeco[1].endereco,
+            dadosEnredeco[1].numero,
+            dadosEnredeco[1].cidade,
+            dadosEnredeco[1].estado,
+            dadosEnredeco[1].cep,
+            dadosEnredeco[1].telefone,
+            dadosEnredeco[1].email
+        )
+        // VALIDAÇÃO
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+    })
+
+})
